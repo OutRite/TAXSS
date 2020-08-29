0baedcd454e208c9ffe68c21a66650a28527181b
OutRite <60903237+OutRite@users.noreply.github.com>
2020-08-28 18:36:33 -0700

    yes

---
+++ taxss.py
@@ -0,0 +1,12 @@
+# https://github.com/OutRite/TAXSS
+
+def sanitize_xss(unfiltered):
+	unfiltered = unfiltered.replace('<', '&lt;')
+	unfiltered = unfiltered.replace('>', '&gt;')
+	return unfiltered
+
+if __name__ == '__main__':
+	print('TAXSS Demo')
+	unfiltered = input('Enter XSS string: ')
+	filtered = sanitize_xss(unfiltered)
+	print(filtered)
